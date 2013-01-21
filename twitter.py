5cf814ac7540d3e8ec59201a9d4e551941f3fd80
pzaytsev <pzaicev@inbox.ru>
2013-01-21 13:59:02 +0400

    Added authication function to twitter.

---
+++ twitter.py
@@ -0,0 +1,37 @@
+'''
+Created on 21.01.2013
+
+@author: pzaytsev
+'''
+import oauth2 as oauth
+import urlparse
+
+def twitter_auth():
+    '''
+    Authorize user in twitter.
+    @author: pzaytsev
+    '''
+    CONSUMER_KEY = 'QhPt0WI3dpVuRcNXyHLJKQ'
+    CONSUMER_SECRET = 'ZnZEVx1BSERrBF4mSO98e02LyGa5SbOe1AA7v0wU'
+    #URLs
+    REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+    ACCESS_TOKEN_URL =  'https://api.twitter.com/oauth/access_token'
+    AUTHORIZE_URL =     'https://api.twitter.com/oauth/authorize'
+    # Create out OAuth consumer instance
+    consumer = oauth.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
+    client = oauth.Client(consumer)
+    resp, content = client.request(REQUEST_TOKEN_URL, "POST", headers = {})
+    request_token = dict(urlparse.parse_qsl(content))
+    print "Go to the following link in your browser:"
+    print "%s?oauth_token=%s" % (AUTHORIZE_URL, request_token['oauth_token'])
+    oauth_verifier = raw_input('What is the PIN? ')
+    token = oauth.Token(request_token['oauth_token'],request_token['oauth_token_secret'])
+    token.set_verifier(oauth_verifier)
+    client = oauth.Client(consumer, token)
+    resp, content = client.request(ACCESS_TOKEN_URL, "POST", headers = {})
+    # parse out the access token
+    access_token = dict(urlparse.parse_qsl(content))
+    # swap in the new auth token to the client
+    token = oauth.Token(key=access_token['oauth_token'],secret=access_token['oauth_token_secret'])
+    client = oauth.Client(consumer, token)
+    return client
